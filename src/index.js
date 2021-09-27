431a5bd3fe1ffab579828d16c8ced6b97da457c5
ktarotaro <88434336+ktarotaro@users.noreply.github.com>
2021-09-27 18:49:27 +0900

    保存

--- src/index.js
+++ src/index.js
@@ -120,5 +120,41 @@
 //   console.log(`こんにちは！${name}さん`);
 // };
 
-const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん`);
-sayHello("じゃけぇ");
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん`);
+// sayHello("じゃけぇ");
+
+/**
+ * スプレッド構文...（ドット3つ）
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// //分割代入
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピーや結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
