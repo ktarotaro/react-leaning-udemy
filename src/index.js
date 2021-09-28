869cc57c72f9da7c2eb4008ac6563a7128c0ff56
ktarotaro <88434336+ktarotaro@users.noreply.github.com>
2021-09-28 14:49:25 +0900

    一時保存

--- src/index.js
+++ src/index.js
@@ -162,11 +162,12 @@
 /**
  * mapやfilterを使った処理
  */
-const nameArr = ["田中", "山田", "じゃけぇ"];
+// const nameArr = ["田中", "山田", "じゃけぇ"];
 // for (let index = 0; index < nameArr.length; index++) {
-//   console.log(nameArr[index]);
+//   console.log(`${index + 1}番目は、${nameArr[index]}です。`);
 // }
 
+//map
 //1.新しい配列を作る
 // const nameArr2 = nameArr.map((name) => {
 //   return name;
@@ -174,4 +175,37 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 // console.log(nameArr2);
 
 //2.順番に処理する
-nameArr.map((name) => console.log(name));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は、${name}です。`));
+
+//filter
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val = 1 < 0 ? "trueです" : "falseです";
+// console.log(val);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です";
+};
+console.log(checkSum(50, 40));
